fdeab27ec40533132ea3ba03056895735af7e937
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-02-14 13:22:22 +0100

    fix

---
+++ 3aAPC/ITL/Aufgabe-2/kap03/uebung01.js
@@ -0,0 +1,6 @@
+var number = 1;
+var text = "Text1";
+var bool = true;
+
+alert(number + " " + text + " " + bool);
+document.write("Beispiel für eine Nummer: " + number +"<br>" + "Beispiel für eine Zeichenkette: " + text + "<br>" + "Beispiel für einen Boolschen Wert: " + bool);
---
+++ 3aAPC/ITL/Aufgabe-2/kap03/uebung02.js
@@ -0,0 +1,3 @@
+document.write("3 < 8:" + 3 < 8 + "<br>" + "true && false" + true && false + "<br>");
+document.write(6 + 4 * 2 - 1);
+document.write("Hund" != "Katze");
