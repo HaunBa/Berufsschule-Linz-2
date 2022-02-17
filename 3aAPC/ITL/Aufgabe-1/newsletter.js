ef03c4d2e17afa9c33b2e202685e6c6fbbff1fa0
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-02-17 11:31:34 +0100

    Update newsletter.js

--- 3aAPC/ITL/Aufgabe-1/newsletter.js
+++ 3aAPC/ITL/Aufgabe-1/newsletter.js
@@ -1,19 +1,20 @@
 window.onload = function(){
     document.getElementById("submit").onclick = submit;
-
-    document.getElementById("logs")
 }
 
 function submit() {
+    var saveErrors = "";
     var inputs = document.getElementsByName("textInput");
     inputs.forEach(element => {
         if (element.value == "") {
+            alert(element.name + " | Eingabe falsch");
             AddError("Text eingeben");
         }else if(element.name == "email"){
-            if (checkEmailAdress(element.value) == false) {
-                
+            if (checkEmailAdress(element.value) == true) {                
                 AddError("Email not valid");
             }
+        }else{
+            alert("everything ok");
         }
     });
 }
@@ -21,9 +22,12 @@ function submit() {
 function AddError(errorText) {
     alert(errorText);
     var p = document.createElement("p");
-        var text = document.createTextNode(errorText);
-        p.appendChild(text);
-        document.getElementById("logs")[0].appendChild(p);
+    var text = document.createTextNode(errorText);
+    p.appendChild(text);
+    document.getElementById("logs")[0].appendChild(p);
+    
+    //saveErrors += errorText + "<br>";
+    //document.getElementById("logs").innerHTML = saveErrors;
 }
 
 function checkEmailAdress(value) {
