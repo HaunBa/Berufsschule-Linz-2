26043824246f41f7658eea6f35adbb8be3dc3a4d
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-02-27 17:54:43 +0100

    hurensohn

--- 3aAPC/ITL/Aufgabe-1/newsletter.js
+++ 3aAPC/ITL/Aufgabe-1/newsletter.js
@@ -1,9 +1,11 @@
+var saveErrors = "";
+
 window.onload = function(){
     document.getElementById("submit").onclick = submit;
 }
 
 function submit() {
-    var saveErrors = "";
+    var errors = 0;
 
     var txt_vname = document.getElementById("vname");
     var txt_nname = document.getElementById("nname");
@@ -11,25 +13,28 @@ function submit() {
 
     if (txt_vname.value == txt_nname.value) {
         AddError("First name cant be the same as the last name.");
+        errors++;
     }else if (checkEmailAdress(txt_email.value) == false) {
         AddError("Email adress is invalid.");
+        errors++;
     }else{
         alert("All good");
+    }
+
+    if (errors != 0) {
+        event.defaultPrevented();
     }
 }
 
 function AddError(errorText) {
-    alert(errorText);
-    var p = document.createElement("p");
-    var text = document.createTextNode(errorText);
-    p.appendChild(text);
-    document.getElementById("logs")[0].appendChild(p);
     
     saveErrors += errorText + "<br>";
     document.getElementById("logs").innerHTML = saveErrors;
+
+    alert(errorText);
 }
 
-function checkEmailAdress(value) {
+function checkEmailAdress(email) {
     var re = /\S+@\S+\.\S+/;
     return re.test(email);
 }
