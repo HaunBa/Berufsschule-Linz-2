3d3d67528f8bf23cec941206561974aac8746e47
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-02-28 16:06:30 +0100

    yeet

---
+++ 3aAPC/ITL/Aufgabe-3/script.js
@@ -0,0 +1,12 @@
+window.onload = function () { getStateOfSwitch();  }
+
+
+function getStateOfSwitch() {
+    if (document.getElementById('switchInput').checked == true) {
+        document.getElementById('list').style.display = 'none';
+        document.getElementById('blogtable').style.display = 'inline';
+    } else {
+        document.getElementById('list').style.display = 'inline';
+        document.getElementById('blogtable').style.display = 'none';
+    }
+}
