565800ce397d600019f337bca3ddadaad05aa970
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-02 09:59:47 +0100

    Added C# Projects

---
+++ 3aAPC/ITL/Cs-Projekte/Aufgabe1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aufgabe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, int> list = new SortedList<string, int>();
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                list.Add(args[i], i);
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+        }
+    }
+}
