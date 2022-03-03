c62b45876ad54b0b196bbacf151c94e2d277d387
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-03 11:06:52 +0100

    updated aufgabe 4

--- 3aAPC/ITL/Aufgabe-3/script.js
+++ 3aAPC/ITL/Aufgabe-3/script.js
@@ -1,5 +1,33 @@
-window.onload = function () { getStateOfSwitch();  }
+window.onload = function () { getStateOfSwitch();}
 
+function fillOptions(){
+    var fontFamilyOption = document.getElementById("select-font");
+    
+
+    var opt = document.createElement('option');
+    opt.value = "test";
+    opt.innerHTML = "test";
+    fontFamilyOption.appendChild(opt);  
+
+    opt = document.createElement('option');
+    opt.value = "arial";
+    opt.innerHTML = "Arial";
+    fontFamilyOption.appendChild(opt);
+
+    opt = document.createElement('option');
+    opt.value = document.getElementById("theme-header").style.fontFamily;
+    opt.innerHTML = document.getElementById("theme-header").style.fontFamily;
+    fontFamilyOption.appendChild(opt);
+
+}
+
+function UpdateH2() {
+    var h2 = document.getElementsByTagName('h2');
+
+    h2.forEach(element => {
+        element.style.fontFamily = document.getElementById('select-font').value;
+    });
+}
 
 function getStateOfSwitch() {
     if (document.getElementById('switchInput').checked == false) {
