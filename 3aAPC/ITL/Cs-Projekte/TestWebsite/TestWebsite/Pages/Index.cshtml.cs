f04ca2c1459348d5f2356a2e89e5da98197dd0f0
HaunBa <44608793+HaunBa@users.noreply.github.com>
2022-03-05 11:41:36 +0100

    edited Github.pptx

--- 3aAPC/ITL/Cs-Projekte/TestWebsite/TestWebsite/Pages/Index.cshtml.cs
+++ 3aAPC/ITL/Cs-Projekte/TestWebsite/TestWebsite/Pages/Index.cshtml.cs
@@ -6,13 +6,19 @@ namespace TestWebsite.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public Timer timer1 { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public Timer GetTimer1()
+        {            
+            return timer1;
+        }
+
+        public void OnGet(Timer timer1)
         {
             Console.WriteLine("Test");
         }
