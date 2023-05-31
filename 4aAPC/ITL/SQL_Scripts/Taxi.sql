ab256efcbef47804754b93b0f4f9eb6ff3df4392
HaunBa <haunschmied.b@gmail.com>
2023-05-31 14:00:39 +0200

    Added Files

---
+++ 4aAPC/ITL/SQL_Scripts/Taxi.sql
@@ -0,0 +1,10 @@
+-- use correct databases
+show databases;
+use taxi;
+
+select u.Uhrzeit_Datum_Beignn, u.Uhrzeit_Datum_Ende, fa.vname, fa.nname, k.Kilometerstand_Beginn, k.Kilometerstand_Ende 
+from fahrt f
+join uhrzeit u on f.uhrzeit_id = u.Id
+join fahrer fa on f.fahrer_id = fa.Id
+join kilometerstand k on f.kilometerstand_id = k.Id
+join auto a on f.auto_id = a.Id;
